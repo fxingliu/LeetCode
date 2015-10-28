728219cd615512441f2046e5a3eedb47f9b9d64e
fxingliu <fxingliu@gmail.com>
2015-10-27 23:58:01 -0700

    Update SymmetricTree.cpp
    iterative (also can be done by two queues)

--- SymmetricTree.cpp
+++ SymmetricTree.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         if (!root) return true;
-        return symmetric(root->left, root->right);
-    }
-    
-    bool symmetric(TreeNode *left, TreeNode *right) {
-        if (!left && !right) return true;
-        if (left && right && left->val==right->val)
-            return (symmetric(left->left, right->right) && symmetric(left->right, right->left));
-        return false;
+        stack<TreeNode*> s;
+        
+        s.push(root->left);
+        s.push(root->right);
+        while (!s.empty()) {
+            TreeNode *r = s.top();
+            s.pop();
+            TreeNode *l = s.top();
+            s.pop();
+            if (!r && !l) continue;
+            if (!r || !l) return false;
+            if (r->val != l->val) return false;
+            
+            s.push(r->right);
+            s.push(l->left);
+            s.push(r->left);
+            s.push(l->right);
+        }
+        return true;
     }
 };
