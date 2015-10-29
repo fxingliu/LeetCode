c87464378b2610d46f3953a05706d8b0fb915f34
fxingliu <fxingliu@gmail.com>
2015-10-28 23:52:37 -0700

    Update ImplementStrStr.cpp
    KMP algorithm (with bug)

--- ImplementStrStr.cpp
+++ ImplementStrStr.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for (int i=0; ;i++) 
-            for (int j=0; ;j++) {
-                if (j == needle.size()) return i;
-                if (i+j == haystack.size()) return -1;
-                if (haystack[i+j] != needle[j]) break;
-            }
+        if (needle.size() == 0) return 0;
+        vector<int> next = cal_next(needle);
+        int i, j=-1;
+        for (i=0; i<haystack.size(); i++) {
+            while (j>-1 && needle[j+1]!=haystack[i])
+                j = next[j];
+            if (needle[j+1] == haystack[i])
+                j++;
+            if (j == needle.size()-1) // match
+                return i-j;
+            j = next[j];
+        }
+        return -1;
+    }
+    
+private:
+    // calculate next[]
+    vector<int> cal_next(string needle) {
+        vector<int> next;
+        int j = -1;
+        // initialize next[0] = -1
+        next.push_back(j);
+        for (int i=1; i<needle.size(); i++) {
+            while (j>-1 && needle[j+1]!=needle[i]) 
+                j = next[j];
+            if (needle[j+1] == needle[i]) j++;
+            next.push_back(j);
+        }
+        return next;
     }
 };
