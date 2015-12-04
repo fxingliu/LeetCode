397432fae6831f7a70ad93cdb4f9c7b8332e34c7
fxingliu <fxingliu@gmail.com>
2015-12-03 20:07:14 -0800

    Update ValidateBinarySearchTree.cpp
    in-order traverse
    check monotonic

--- ValidateBinarySearchTree.cpp
+++ ValidateBinarySearchTree.cpp
@@ -10,13 +10,19 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return checkBST(root, NULL, NULL);
+        prev = NULL;
+        return checkBST(root);
     }
-    
+
 private:
-    bool checkBST(TreeNode *t, TreeNode *min, TreeNode *max) {
+    TreeNode* prev;
+    bool checkBST(TreeNode *t) {
         if (!t) return true;
-        if (min && t->val <= min->val || max && t->val >= max->val) return false;
-        return checkBST(t->left, min, t) && checkBST(t->right, t, max);
+        if (checkBST(t->left)) {
+            if (prev && t->val <= prev->val) return false;
+            prev = t;
+            return checkBST(t->right);
+        }
+        return false;
     }
 };
