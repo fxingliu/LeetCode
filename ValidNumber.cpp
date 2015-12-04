35f75efd68bf3132225c9e310d3f4a8104513ffe
fxingliu <fxingliu@gmail.com>
2015-12-04 11:54:18 -0800

    Update ValidNumber.cpp
    straight-forward processing
    isNum flag

--- ValidNumber.cpp
+++ ValidNumber.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
     bool isNumber(string s) {
-        enum s_type {
-            INVALID,
-            SPACE,
-            SIGN,
-            DIGIT,
-            DOT,
-            EXP,
-            NUM_TYPE
-        };
-        
-        const int transition[][NUM_TYPE] = {
-            -1, 0, 3, 1, 2, -1,
-            -1, 8, -1, 1, 4, 5,
-            -1, -1, -1, 4, -1, -1,
-            -1, -1, -1, 1, 2, -1,
-            -1, 8, -1, 4, -1, 5,
-            -1, -1, 6, 7, -1, -1,
-            -1, -1, -1, 7, -1, -1,
-            -1, 8, -1, 7, -1, -1,
-            -1, 8, -1, -1, -1, -1,
-        };
-        
-        int state = 0;
-        for (char& c : s) {
-            s_type type = INVALID;
-            
-            if (isspace(c)) type = SPACE;
-            else if (c == '+' || c == '-') type = SIGN;
-            else if (isdigit(c)) type = DIGIT;
-            else if (c == '.') type = DOT;
-            else if (c == 'E' || c == 'e') type = EXP;
-            
-            state = transition[state][type];
-            if (state==-1) 
-                return false;
+        int i = 0, n = s.size();
+        while (i<n && isspace(s[i])) i++;
+        if (i<n && (s[i]=='+' || s[i]=='-')) i++;
+        bool isNum = false;
+        while (i<n && isdigit(s[i])) {
+          i++;
+          isNum = true;
         }
+        if (i<n && s[i]=='.') {
+            i++;
+            while (i<n && isdigit(s[i])) {
+                i++;
+                isNum = true;
+            }
+        }
+        if (isNum && i<n && (s[i]=='E' || s[i]=='e')) {
+            i++;
+            if (s[i]=='+' || s[i]=='-') i++;
+            isNum = false;
+            while (i<n && isdigit(s[i])) {
+                i++;
+                isNum = true;
+            }
+        }
+        while (i<n && isspace(s[i])) i++;
         
-        if (state==1 || state==4 || state==7 || state==8)
-            return true;
-        return false;
+        return isNum && i==n;
     }
 };
