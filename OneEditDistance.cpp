7f893b88ce457fc0b8b7912f739f321b0ddd4850
fxingliu <fxingliu@gmail.com>
2015-12-07 00:43:00 -0800

    Update OneEditDistance.cpp
    merge two cases into one

--- OneEditDistance.cpp
+++ OneEditDistance.cpp
@@ -2,38 +2,13 @@ class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
         int m = s.size(), n = t.size();
-        if (m > n) return checkInsert(t, s);
-        else if (m < n) return checkInsert(s, t);
-        return checkReplace(s, t);
-    }
-    
-private:
-    // assume len(s)<len(t)
-    bool checkInsert(string s, string t) {
-        if (t.size() - s.size() != 1) return false;
-        int i = 0, j = 0;
-        while (i < s.size()) {
-            if (s[i] != t[j]) {
-                if (i != j) return false;
-                j++;
-            }
-            else {
-                i++;
-                j++;
-            }
-        }
-        return true;
-    }
-    
-    // assume len(s)==len(t)
-    bool checkReplace(string s, string t) {
-        bool found = false;
-        for (int i=0; i<s.size(); i++) {
-            if (s[i] != t[i]) {
-                if (found) return false;
-                found = true;
-            }
-        }
-        return found;
+        if (m > n) return isOneEditDistance(t, s);
+        int shift = n-m, i = 0;
+        if (shift > 1) return false;
+        while (i<m && s[i]==t[i]) i++;
+        if (i == m) return m != n;
+        if (m == n) i++;
+        while (i<m && s[i]==t[i+shift]) i++;
+        return i == m;
     }
 };
