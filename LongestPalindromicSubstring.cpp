52a07903d00e9a5976ecfff47d98091e8e1f2666
fxingliu <fxingliu@gmail.com>
2015-12-06 19:51:05 -0800

    Update LongestPalindromicSubstring.cpp
    expand around center
    O(1) space

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int len = s.size(), start = 0, end = 0;
-        // indicate whether s[i->j] is palindrome
-        bool flag[len][len];
-        memset(flag, 0, len*len*sizeof(bool));
-        
-        for (int i=len-1; i>=0; i--) 
-            for (int j=i; j<len; j++) {
-                if (i == j) flag[i][j] = true;
-                else if (i+1 == j) flag[i][j] = s[i] == s[j];
-                else flag[i][j] = s[i]==s[j] && flag[i+1][j-1];
-                
-                if (flag[i][j] && j-i > end-start) {
-                    start = i;
-                    end = j;
-                }
+        if (len < 2) return s;
+        for (int i=0; i<len; i++) {
+            int len1 = centric(s, i, i);
+            int len2 = centric(s, i, i+1);
+            int len = max(len1, len2);
+            if (len > end-start+1) {
+                start = i - (len-1) / 2;
+                end = i + len / 2;
             }
-                
+        }
         return s.substr(start, end-start+1);
+    }
+    
+private:
+    int centric(string s, int L, int R) {
+        while (L >= 0 && R < s.size()) {
+            if (s[L] != s[R]) break;
+            L--;
+            R++;
+        }
+        return R-L-1;
     }
 };
