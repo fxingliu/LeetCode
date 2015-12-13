92bad3e4dba6ac50bc85471b3edcd7c705ae54d3
fxingliu <fxingliu@gmail.com>
2015-12-12 16:30:33 -0800

    Update BinaryTreePostorderTraversal.cpp
    iterative
    the reverse of preorder traversal

--- BinaryTreePostorderTraversal.cpp
+++ BinaryTreePostorderTraversal.cpp
@@ -7,33 +7,23 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-struct NodeWrapper {
-    TreeNode *node;
-    int visited;
-    NodeWrapper(TreeNode *p) : node(p), visited(0) {}
-};
-
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ret;
         if (!root) return ret;
-        stack<NodeWrapper> s;
-        NodeWrapper p(root);
-        s.push(p);
+        stack<TreeNode*> s;
+        s.push(root);
         
         while (!s.empty()) {
-            p = s.top();
+            TreeNode *p = s.top();
             s.pop();
-            if (++p.visited == 3) {
-                ret.push_back(p.node->val);
-                continue;
-            }
-            s.push(p);
-            if (p.visited == 1 && p.node->left) s.push(NodeWrapper(p.node->left));
-            if (p.visited == 2 && p.node->right) s.push(NodeWrapper(p.node->right));
+            ret.push_back(p->val);
+            if (p->left) s.push(p->left);
+            if (p->right) s.push(p->right);
         }
         
+        reverse(ret.begin(), ret.end());
         return ret;
     }
 };
