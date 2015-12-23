6f17ed9117e39a779d3be65e6e42abade184358e
fxingliu <fxingliu@gmail.com>
2015-12-23 15:13:22 -0800

    Update Sqrt(x).cpp
    bit manipulation
    O(1) time

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) return 0;
-        int L = 1, R = x;
-        while (true) {
-            // avoid overflow
-            int mid = L + (R-L) / 2;
-            if (mid > x/mid) 
-                R = mid;
-            else {
-                if (mid+1 <= x/(mid+1)) L = mid+1;
-                else return mid;
-            }
-        }
+        int res = 0;
+        for (int i=15; i>=0; i--) 
+            // precedence: '+' larger than '<<'
+            if ((res + (1<<i)) <= x / (res + (1<<i))) 
+                res += 1<<i;
+        return res;
     }
 };
