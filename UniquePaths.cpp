8f29e11cb5e1fedcf0d99abfbff48f05ccca8162
fxingliu <fxingliu@gmail.com>
2015-12-26 13:27:03 -0800

    Update UniquePaths.cpp
    reduce space complexity to O(min(m, n))

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> path(m, vector<int>(n, 1));
-        for (int i=1; i<m; i++)
-            for (int j=1; j<n; j++) 
-                path[i][j] = path[i-1][j] + path[i][j-1];
-        return path[m-1][n-1];
+        if (m > n) return uniquePaths(n, m);
+        vector<int> cur(m, 1);
+        for (int j=1; j<n; ++j)
+            for (int i=1; i<m; ++i)
+                cur[i] += cur[i-1];
+        return cur[m-1];
     }
 };
