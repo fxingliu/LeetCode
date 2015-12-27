9dfb87669ef32d8f76a7f6fac697777dfcbfbef1
fxingliu <fxingliu@gmail.com>
2015-12-26 23:53:36 -0800

    Update Permutations.cpp
    recursive
    DFS
    O(n!) time

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         vector<vector<int>> ret;
-        do {
-            ret.push_back(nums);
-        }
-        while (nextPermute(nums));
+        dfs(nums, 0, ret);
         return ret;
     }
     
 private:
-    bool nextPermute(vector<int>& nums) {
-        int i = nums.size()-1, j = nums.size()-1;
-        while (i>0 && nums[i-1] >= nums[i]) i--;
-        if (i == 0) return false;
-        while (nums[j] <= nums[i-1]) j--;
-        swap(nums[j], nums[i-1]);
-        reverse(nums.begin()+i, nums.end());
-        return true;
+    void dfs(vector<int>& nums, int pos, vector<vector<int>>& ret) {
+        if (pos == nums.size()) {
+            ret.push_back(nums);
+            return;
+        }
+        for (int begin=pos; begin<nums.size(); ++begin) {
+            swap(nums[pos], nums[begin]);
+            dfs(nums, pos+1, ret);
+            swap(nums[pos], nums[begin]);
+        }
     }
 };
