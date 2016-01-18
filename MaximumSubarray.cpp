826b479f958e2920a87c77a17fafe1c4b7ec7b6e
fxingliu <fxingliu@gmail.com>
2016-01-17 17:50:02 -0800

    Update MaximumSubarray.cpp
    DP
    O(n) time, O(1) space

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        return sub(nums, 0, nums.size()-1);
-    }
-    
-private:
-    int sub(vector<int>& nums, int L, int R) {
-        if (L == R) return nums[L];
-        int M = (L+R)/2;
-        int leftSub = sub(nums, L, M);
-        int rightSub = sub(nums, M+1, R);
-        
-        int leftPart = INT_MIN, rightPart = INT_MIN, sum = 0;
-        for (int i=M; i>=L; --i) {
+        int maxSum = INT_MIN, sum = 0;
+        for (int i=0; i<nums.size(); ++i) {
             sum += nums[i];
-            if (sum > leftPart) leftPart = sum;
+            if (sum > maxSum) maxSum = sum;
+            if (sum < 0) sum = 0;
         }
-        sum = 0;
-        for (int i=M+1; i<=R; ++i) {
-            sum += nums[i];
-            if (sum > rightPart) rightPart = sum;
-        }
-        sum = leftPart + rightPart;
-        return max(sum, max(leftSub, rightSub));
+        return maxSum;
     }
 };
