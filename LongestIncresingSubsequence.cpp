e172399a411daf0f7f83e6d2f5a358f421d71221
fxingliu <fxingliu@gmail.com>
2016-01-22 17:12:21 -0800

    Update LongestIncresingSubsequence.cpp
    another DP solution using binary search
    O(nlogn) time

--- LongestIncresingSubsequence.cpp
+++ LongestIncresingSubsequence.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        const int n = nums.size();
-        vector<int> lis(n, 1);
-        for (int i=1; i<n; ++i) 
-            for (int j=0; j<i; ++j) 
-                if (nums[j] < nums[i] && lis[j]+1 > lis[i]) lis[i] = lis[j]+1;
-        int max_len = 1;
-        for (int j=0; j<n; ++j)
-            if (lis[j] > max_len) max_len = lis[j];
-        return max_len;
+        // last[i] is the minimum value that a subsequence of length i+1 might end with
+        vector<int> last(nums.size());
+        int len = 0;
+        for (int n : nums) {
+            int pos = binarySearch(last, 0, len, n);
+            last[pos] = n;
+            if (pos == len) len++;
+        }
+        return len;
+    }
+    
+private:
+    int binarySearch(vector<int>& v, int begin, int end, int val) {
+        while (begin < end) {
+            int mid = (begin+end)/2;
+            if (val < v[mid]) end = mid;
+            else if (v[mid] < val) begin = mid+1;
+            else return mid;
+        }
+        return begin;
     }
 };
