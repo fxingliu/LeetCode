605911a65ea8c960ea2f46eeb8f1059e359c47bf
fxingliu <fxingliu@gmail.com>
2016-01-23 21:45:49 -0800

    Update MinimumSizeSubarraySum.cpp
    O(nlogn)
    binary search
    虽然不好写可是它慢啊……

--- MinimumSizeSubarraySum.cpp
+++ MinimumSizeSubarraySum.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int begin = 0, end = 0, sum = 0, minLen = INT_MAX;
-        while (end<nums.size()) {
-            sum += nums[end++];
-            while (sum >= s) {
-                minLen = min(minLen, end-begin);
-                sum -= nums[begin++];
+        vector<int> sum(nums.size()+1);
+        getSum(nums, sum);
+        int minLen = INT_MAX;
+        for (int end=1; end<=nums.size(); ++end) {
+            if (sum[end] >= s) {
+                // the first element such that sum[begin] > sum[end] - s
+                int begin = binarySearch(sum, sum[end]-s);
+                if (begin <= end) minLen = min(minLen, end-begin+1);
             }
         }
         return minLen == INT_MAX ? 0 : minLen;
+    }
+    
+private:
+    // construct the accumulate sum array
+    void getSum(vector<int>& nums, vector<int>& sum) {
+        sum[0] = 0;
+        for (int i=0; i<nums.size(); ++i)
+            sum[i+1] = sum[i] + nums[i];
+    }
+    
+    int binarySearch(vector<int>& v, int target) {
+        int L = 0, R = v.size()-1;
+        while (L < R) {
+            int M = (L+R)/2;
+            if (v[M] < target) L = M+1;
+            else R = M;
+        }
+        return v[L] <= target ? L+1 : L;
     }
 };
