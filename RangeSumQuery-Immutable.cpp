b1e353c60cb3bec4d4b82694d61f75264e34ef17
fxingliu <fxingliu@gmail.com>
2016-01-24 16:19:30 -0800

    Create RangeSumQuery-Immutable.cpp
    O(n) initial, O(1) query time
    O(n) extra space

---
+++ RangeSumQuery-Immutable.cpp
@@ -0,0 +1,22 @@
+class NumArray {
+public:
+    NumArray(vector<int> &nums) {
+        sum.resize(nums.size()+1);
+        sum[0] = 0;
+        for (int i=0; i<nums.size(); i++)
+            sum[i+1] = sum[i] + nums[i];
+    }
+
+    int sumRange(int i, int j) {
+        return sum[j+1] - sum[i];
+    }
+    
+private:
+    vector<int> sum;
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
