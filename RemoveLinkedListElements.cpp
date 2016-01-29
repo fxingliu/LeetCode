3684d1375781e114bc810846a9e7b2b17c2508e3
fxingliu <fxingliu@gmail.com>
2016-01-29 10:55:14 -0800

    Update RemoveLinkedListElements.cpp
    recursive
    more concise

--- RemoveLinkedListElements.cpp
+++ RemoveLinkedListElements.cpp
@@ -9,17 +9,8 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode *p = &dummy, *q = head;
-        while (q) {
-            if (q->val != val) {
-                p->next = q;
-                p = q;
-            }
-            q = q->next;
-        }
-        p->next = q;
-        return dummy.next;
+        if (!head) return NULL;
+        head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
     }
 };
