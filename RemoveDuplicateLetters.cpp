fca45b0a52ed4ead99b247e991d8868f80bdb1d4
fxingliu <fxingliu@gmail.com>
2016-01-29 23:59:02 -0800

    Update RemoveDuplicateLetters.cpp
    iterative
    use the string ret as a stack
    O(n) time

--- RemoveDuplicateLetters.cpp
+++ RemoveDuplicateLetters.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        if (s.empty()) return "";
-        vector<int> count(26, 0);
-        int pos = 0;
-        for (char c : s) count[c-'a']++;
-        for (int i=0; i<s.size(); i++) {
-            if (s[i] < s[pos]) pos = i;
-            if (--count[s[i]-'a'] == 0) break;
-        }
-        return s.at(pos) + removeDuplicateLetters(strip(s.substr(pos+1), s[pos]));
-    }
-    
-private:
-    string strip(string s, char key) {
-        if (s.empty()) return "";
-        int idx = 0;
+        int count[26] = {0};
+        vector<bool> inResult(26, false);
         string ret;
-        for (char c : s) 
-            if (c != key) ret.push_back(c);
+        
+        for (char c : s) count[c-'a']++;
+        for (char c : s) {
+            count[c-'a']--;
+            if (inResult[c-'a']) continue;
+            while (!ret.empty() && c < ret.back() && count[ret.back()-'a']) {
+                inResult[ret.back()-'a'] = false;
+                ret.pop_back();
+            }
+            ret.push_back(c);
+            inResult[c-'a'] = true;
+        }
+        
         return ret;
     }
 };
