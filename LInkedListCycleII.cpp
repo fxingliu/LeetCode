c87f4cc09faf9fc6765d503c1508a7900c76002c
fxingliu <fxingliu@gmail.com>
2016-01-30 11:37:08 -0800

    Create LInkedListCycleII.cpp
    two pointers
    O(n) time, O(1) space

---
+++ LInkedListCycleII.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *p = head, *q = head;
+        while (q && q->next) {
+            p = p->next;
+            q = q->next->next;
+            if (p == q) break;
+        }
+        if (!q || !q->next) return NULL;
+        
+        q = head;
+        while (p != q) {
+            p = p->next;
+            q = q->next;
+        }
+        return q;
+    }
+};
