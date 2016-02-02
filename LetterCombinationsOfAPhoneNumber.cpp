2520926b9b3f9692fa560582c74026dde47657e8
fxingliu <fxingliu@gmail.com>
2016-02-01 20:38:07 -0800

    Update LetterCombinationsOfAPhoneNumber.cpp
    BFS; iterative; use queue

--- LetterCombinationsOfAPhoneNumber.cpp
+++ LetterCombinationsOfAPhoneNumber.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ret;
-        if (digits.empty()) return ret;
-        string path;
-        dfs(ret, digits, path, 0);
-    }
-    
-private:
-    vector<string> map{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    
-    void dfs(vector<string>& ret, string& digits, string& path, int begin) {
-        if (begin == digits.size()) {
-            ret.push_back(path);
-            return;
+        vector<string> map{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits.empty()) return vector<string>();
+        
+        deque<string> ret;
+        ret.push_back("");
+        while (ret.front().size() < digits.size()) {
+            string cur = ret.front();
+            ret.pop_front();
+            string letter = map[digits[cur.size()]-'0'];
+            for (char c : letter)
+                ret.push_back(cur+c);
         }
-        string letter = map[digits[begin]-'0'];
-        for (char c : letter) {
-            path.push_back(c);
-            dfs(ret, digits, path, begin+1);
-            path.pop_back();
-        }
+        
+        return vector<string>(ret.begin(), ret.end());
     }
 };
