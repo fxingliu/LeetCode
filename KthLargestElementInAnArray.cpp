35539b7e618b7d1ca761be89d9a5b89c8cb5a6bf
fxingliu <fxingliu@gmail.com>
2016-02-03 20:24:38 -0800

    QuickSort source code
    sort directly
    O(nlgn) time, O(1) space
    concise quicksort

--- KthLargestElementInAnArray.cpp
+++ KthLargestElementInAnArray.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int left = 0, right = nums.size()-1;    
-        while (1) {
-            int ret = partition(nums, left, right);
-            if (ret == k-1) return nums[ret];
-            else if (ret < k-1) left = ret + 1;
-            else right = ret - 1;
-        }
+        quickSort(nums, 0, nums.size()-1);
+        return nums[nums.size()-k];
     }
     
 private:
-    int partition(vector<int>& nums, int left, int right) {
-        if (left == right) return left;
-        int pivot = nums[left], L = left+1, R = right;
+    int quickSort(vector<int>& nums, int left, int right) {
+        int pivot = nums[(left+right)/2], L = left, R = right;
         while (L <= R) {
-            if (nums[L] < pivot && nums[R] > pivot) swap(nums[L++], nums[R--]);
-            if (nums[L] >= pivot) L++;
-            if (nums[R] <= pivot) R--;
+            while (nums[L] < pivot) L++;
+            while (nums[R] > pivot) R--;
+            if (L <= R) swap(nums[L++], nums[R--]);
         }
-        swap(nums[left], nums[R]);
-        return R;
+        if (L < right) quickSort(nums, L, right);
+        if (R > left) quickSort(nums, left, R);
     }
 };
