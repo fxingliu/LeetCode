91cb4d5c131ed061282e8ffe006a20b6ecae279e
fxingliu <fxingliu@gmail.com>
2016-02-12 15:14:56 -0800

    Update NumberOfDigitOne.cpp
    count from right to left is simpler
    another way to avoid overflow is for(long long digit = 1;

--- NumberOfDigitOne.cpp
+++ NumberOfDigitOne.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int countDigitOne(int n) {
-        if (n <= 0) return 0;
-        int digit = 1, count = 0;
-        // (digit*10 <= n) may lead to overflow
-        while (digit <= n/10) digit *= 10;
-        while (digit) {
-            int pre = n/digit/10, cur = n/digit%10;
-            if (cur == 0) 
-                count += pre*digit;
-            else if (cur == 1) 
-                count += pre*digit + n%digit + 1;
-            else 
-                count += (pre+1)*digit;
-            digit /= 10;
+        int count = 0;
+        for (int digit=1; digit<=n; digit*=10) {
+            int prev = n/digit, next = n%digit;
+            if (prev%10 < 1) count += prev/10*digit;
+            else if (prev%10 == 1) count += prev/10*digit + next + 1;
+            else count += (prev/10+1)*digit;
+            // avoid overflow
+            if (digit > INT_MAX/10) break;
         }
         return count;
     }
