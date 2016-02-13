df444c45a8e1484b6d92e55253838839d7ea42fc
fxingliu <fxingliu@gmail.com>
2016-02-13 11:57:19 -0800

    Update MaximalSquare.cpp
    reduce the extra space to two vectors or one
    O(mn) time, O(n) space

--- MaximalSquare.cpp
+++ MaximalSquare.cpp
@@ -4,22 +4,19 @@ public:
         if (matrix.empty() || matrix[0].empty()) return 0;
         
         const int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int maxLen = 0;
-        for (int i=0; i<m; i++) {
-            dp[i][0] = matrix[i][0]-'0';
-            if (dp[i][0]) maxLen = 1;
-        }
-        for (int j=0; j<n; j++) {
-            dp[0][j] = matrix[0][j]-'0';
-            if (dp[0][j]) maxLen = 1;
-        }
+        // pad a zero at the beginning
+        vector<int> dp(n+1);
+        int maxLen = 0, pre = 0;
         
-        for (int i=1; i<m; i++)
-            for (int j=1; j<n; j++) {
-                if (matrix[i][j] == '0') continue;
-                dp[i][j] = min(min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]) + 1;
-                maxLen = max(maxLen, dp[i][j]);
+        for (int i=0; i<m; i++)
+            for (int j=1; j<=n; j++) {
+                int tmp = dp[j];
+                if (matrix[i][j-1] == '0') dp[j] = 0;
+                else {
+                    dp[j] = min(min(dp[j], dp[j-1]), pre) + 1;
+                    maxLen = max(maxLen, dp[j]);
+                }
+                pre = tmp;
             }
             
         return maxLen*maxLen;
