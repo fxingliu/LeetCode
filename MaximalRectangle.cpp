8c708bbda9f4ddab0b7209536e3424359481439b
fxingliu <fxingliu@gmail.com>
2016-02-13 16:40:58 -0800

    Update MaximalRectangle.cpp
    stack, the same idea as LargestRectangleInHistogram
    in each row, use a vector to store the height and a stack to find the maximal area
    O(mn) time, O(n) space

--- MaximalRectangle.cpp
+++ MaximalRectangle.cpp
@@ -3,34 +3,24 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         if (matrix.empty()) return 0;
         const int m = matrix.size(), n = matrix[0].size();
-        // area = (R-L)*H
-        // R scan from right to left, L from left to right. H can be from either side
-        vector<int> L(n, 0), R(n, n), H(n, 0);
+        // H[j] is the current height at [i][j]. H[n] = 0 as a sentinel
+        vector<int> H(n+1);
         int maxArea = 0;
         
         for (int i=0; i<m; i++) {
-            int left = 0, right = n;
+            stack<int> s;
             for (int j=0; j<n; j++) {
-                if (matrix[i][j] == '1') {
-                    L[j] = max(L[j], left);
-                    H[j]++;
-                }
-                else {
-                    L[j] = 0;
-                    H[j] = 0;
-                    left = j+1;
-                }
+                if (matrix[i][j] == '0') H[j] = 0;
+                else H[j]++;
             }
-            
-            for (int j=n-1; j>=0; j--) {
-                if (matrix[i][j] == '1') {
-                    R[j] = min(R[j], right);
-                    maxArea = max(maxArea, (R[j]-L[j])*H[j]);
+            for (int j=0; j<=n; j++) {
+                while (!s.empty() && H[s.top()] > H[j]) {
+                    int cur = s.top();
+                    s.pop();
+                    int k = s.empty() ? -1 : s.top();
+                    maxArea = max(maxArea, (j-k-1)*H[cur]);
                 }
-                else {
-                    R[j] = n;
-                    right = j;
-                }
+                s.push(j);
             }
         }
         return maxArea;
