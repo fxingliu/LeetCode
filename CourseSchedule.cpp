311f49fa8449c4e6064d3f37c5be6cc411ced558
fxingliu <fxingliu@gmail.com>
2016-02-16 17:23:02 -0800

    Update CourseSchedule.cpp
    DFS topological sort
    use the current path to detect cycle
    O(V+E) time (runtime can be reduced by transforming edge list into adjacent matrix)

--- CourseSchedule.cpp
+++ CourseSchedule.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-        unordered_map<int, int> inDegree;
-        queue<int> q;
-        int count = 0;
+        vector<bool> visited(numCourses);
+        vector<bool> current(numCourses);
+        bool hasCycle = false;
         
-        for (auto& i : prerequisites) inDegree[i.first]++;
-        for (int i=0; i<numCourses; i++)
-            if (inDegree[i] == 0) q.push(i);
+        for (int i=0; i<numCourses; i++) 
+            dfs(i, visited, current, prerequisites, hasCycle);
+        return !hasCycle;
+    }
+    
+private:
+    void dfs(int i, vector<bool>& visited, vector<bool>& current, vector<pair<int, int>>& pre, bool& hasCycle) {
+        if (current[i]) hasCycle = true;
+        if (visited[i]) return;
         
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (auto& i : prerequisites) 
-                if (i.second == cur && --inDegree[i.first] == 0) q.push(i.first);
-            count++;
-        }
-        return count == numCourses;
+        visited[i] = true;
+        current[i] = true;
+        for (auto& p : pre) 
+            if (p.second == i) dfs(p.first, visited, current, pre, hasCycle);
+        current[i] = false;
     }
 };
